f94be3e907dfef4ade323f3e3010e74136d3c6ea
Chandu-Develop <bhuvanchandumidde3@gmail.com>
2024-07-24 20:02:11 +0530

    individual project are ready to add

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,39 +3,43 @@ import { useState } from 'react';
 
 
 const Projects = () => {
-  const projects = ['Project1', 'Project2', 'Project3'];
-  // const projects = ['Project1']
+  const projects = ['QuizGuru', 'React Portfolio', 'Weather Website', 'Image BgRemover', 'My Portfolio'];
   const navMenu = [...Array(projects.length).keys()].map(i => i + 1);
 
   const [activeTab, setActiveTab] = useState(1);
-  const [activeProject, setActiveProject] = useState('project1');
+  const [activeProject, setActiveProject] = useState('quizguru');
 
   return (
     <section id='projects'>
-      <div className="container">
+      <div className="wrapper">
         <ol className='project-tabs'>{
           navMenu.map(ind => {
             return (
               <li key={ind}>
-                <a href={`#${activeProject}`}
+                <a href={`#${activeProject.split(' ').join('')}`}
                   className={`tab ${activeTab === ind ? 'active' : ''}`}
                   onClick={() => {
                     setActiveTab(ind);
                     setActiveProject(projects[ind - 1].toLowerCase())
                   }}>
-                  <span></span>
-                  {ind}
+                  <span className="top-hc"></span>
+                  <span className='circle'></span>
+                  <span className="bottom-hc"></span>
+                  {/* {ind} */}
                 </a>
               </li>)
           })}
         </ol>
         <ul className="project-list">
-          {projects.map((project, index) => (
-            <li key={index}
-              className={`${activeProject === project.toLowerCase() ? 'active' : ''}`}>
-              <Project id={project.toLowerCase()} />
-            </li>
-          ))}
+          {projects.map((project, index) => {
+            let id = project.split(' ').join('').toLowerCase();
+            return (
+              <li key={index}
+                className={`${activeProject === project.toLowerCase() ? 'active' : ''}`}>
+                <Project id={id} counter={index + 1} projectTitle={project} />
+              </li>
+            )
+          })}
         </ul>
       </div>
     </section>
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,16 +1,23 @@
 import PropTypes from 'prop-types'
 
-const Project = ({ id }) => {
+const Project = ({ id, counter, projectTitle }) => {
     return (
-        <section id={id} className='project' >
-            {id}
+        <section id={id} className='project'>
+            <div className="container">
+                <h1 className="title">{projectTitle}</h1>
+                <div className="img-c">
+                    <img src="" alt="" />
+                </div>
+                <h1 className='counter'>{`0${counter}`}</h1>
+            </div>
         </section>
     )
 }
 
 Project.propTypes = {
     id: PropTypes.string,
-    project: PropTypes.string
+    counter: PropTypes.number,
+    projectTitle: PropTypes.string
 }
 
 export default Project
